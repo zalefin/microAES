3dad6b8041bc7becb2b133157c2c15447467d99f
Zach Lefin <zachlefin@gmail.com>
2020-07-30 17:54:37 -0400

    implement title scene

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import './App.css';
+import {motion} from 'framer-motion';
 
 class TitleScene extends React.Component {
+    constructor(props) {
+	super(props);
+
+    }
 
     render() {
 	return (
-	    <div>
-	    <div>microAES</div>
-	    <button>encrypt</button>
-	    <button>decrypt</button>
+	    <div className='scene'>
+	    <motion.div className='title'
+		initial={{opacity: 0}}
+		animate={{opacity: 1, transition: {duration: 1.5}}}
+	    >microAES</motion.div>
+	    <motion.div
+		className='button'
+		onClick={this.props.encryptHandler}
+		initial={{opacity: 0}}
+		animate={{opacity: 1, transition: {duration: 1.5, delay: 1}}}
+		whileHover={{scale: 1.05, transition: {duration: 0.5}}}
+		whileTap={{scale: 0.9}}>
+		encrypt
+	    </motion.div>
+	    <motion.div
+		className='button'
+		initial={{opacity: 0}}
+		animate={{opacity: 1, transition: {duration: 1.5, delay: 1.2}}}
+		whileHover={{scale: 1.05, transition: {duration: 0.5}}}
+		whileTap={{scale: 0.9}}>
+		decrypt
+	    </motion.div>
 	    </div>
 
-	)
+	);
     }
 }
 
@@ -25,11 +48,16 @@ class CipherStory extends React.Component {
 
     render() {
 	let scene;
-	if(this.state.scene == "title") {
-	    scene = <TitleScene/>;
+	switch(this.state.scene) {
+	    case 'title':
+		scene = <TitleScene encryptHandler={() => this.setState({scene: "encrypt"})}/>;
+		break;
+	    case 'input':
+		scene = <InputScene />;
+		break;
 	}
 	return (
-	    <div>
+	    <div className='story'>
 	    {scene}
 	    </div>
 	);
