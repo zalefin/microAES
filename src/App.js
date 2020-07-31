ebb3ef08dc1dab12498dd442d608454f418f92e3
Zach Lefin <zachlefin@gmail.com>
2020-07-30 23:31:16 -0400

    add data input scene

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React from 'react';
 import './App.css';
 import {motion} from 'framer-motion';
 
+const smoothButton = function(onClick, text, delay) {
+    return (
+	<motion.div
+	className='button'
+	onClick={onClick}
+	initial={{opacity: 0}}
+	animate={{opacity: 1, transition: {duration: 1.5, delay: delay}}}
+	whileHover={{scale: 1.05, transition: {duration: 0.5}}}
+	whileTap={{scale: 0.9}}>
+	{text}
+	</motion.div>
+    );
+}
+
 class TitleScene extends React.Component {
     constructor(props) {
 	super(props);
@@ -15,25 +29,60 @@ class TitleScene extends React.Component {
 		initial={{opacity: 0}}
 		animate={{opacity: 1, transition: {duration: 1.5}}}
 	    >microAES</motion.div>
-	    <motion.div
-		className='button'
-		onClick={this.props.encryptHandler}
-		initial={{opacity: 0}}
-		animate={{opacity: 1, transition: {duration: 1.5, delay: 1}}}
-		whileHover={{scale: 1.05, transition: {duration: 0.5}}}
-		whileTap={{scale: 0.9}}>
-		encrypt
-	    </motion.div>
-	    <motion.div
-		className='button'
-		initial={{opacity: 0}}
-		animate={{opacity: 1, transition: {duration: 1.5, delay: 1.2}}}
-		whileHover={{scale: 1.05, transition: {duration: 0.5}}}
-		whileTap={{scale: 0.9}}>
-		decrypt
-	    </motion.div>
+	    {smoothButton(this.props.encryptHandler, 'encrypt', 1)}
+	    {smoothButton(this.props.decryptHandler, 'decrpyt', 1.2)}
 	    </div>
 
+	);
+    }
+}
+
+class DataForm extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    data: '',
+	    key: '',
+	};
+
+	this.handleChangeData = this.handleChangeData.bind(this);
+	this.handleChangeKey = this.handleChangeKey.bind(this);
+	this.handleSubmit = this.props.handleSubmit.bind(this);
+    }
+
+    handleChangeData(event) {
+	this.setState({data: event.target.value});
+    }
+
+    handleChangeKey(event) {
+	this.setState({key: event.target.value});
+    }
+
+    render() {
+	return (
+	    <div>
+	    <input id='datain' type="text" value={this.state.data} onChange={this.handleChangeData} />
+	    <br/>
+	    <input id='keyin' type="text" value={this.state.key} onChange={this.handleChangeKey} />
+	    {smoothButton(this.handleSubmit, 'begin', 1)}
+	    </div>
+	);
+    }
+}
+
+class InputScene extends React.Component {
+    constructor(props) {
+	super(props)
+    }
+    render() {
+	return (
+	    <span>
+	    <div>&#x1F82C</div>
+	    <div className='scene'>
+	    <DataForm handleSubmit={this.props.beginHandler}/>
+	    </div>
+	    <div>&#x1F82E</div>
+	    </span>
 	);
     }
 }
@@ -50,10 +99,12 @@ class CipherStory extends React.Component {
 	let scene;
 	switch(this.state.scene) {
 	    case 'title':
-		scene = <TitleScene encryptHandler={() => this.setState({scene: "encrypt"})}/>;
+		scene = <TitleScene encryptHandler={() => this.setState({scene: 'input'})}/>;
 		break;
 	    case 'input':
-		scene = <InputScene />;
+		scene = <InputScene beginHandler={() => this.setState({scene: 'firstop'})}/>;
+		break;
+	    case 'firstop':
 		break;
 	}
 	return (
