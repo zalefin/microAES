179329362b51fe21227d93223ef51aec5ea497a8
Zach Lefin <zachlefin@gmail.com>
2020-08-03 19:02:00 -0400

    add viewcontroller component hold view states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import './App.css';
-import CipherStory from './CipherStory';
+import TitleView from './TitleView';
+import InputView from './InputView';
+import StoryView from './StoryView';
+
+class ViewController extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {view: 'title'};
+    }
+
+    render() {
+	let view;
+	switch(this.state.view) {
+	    case 'title':
+		view = <TitleView
+		encryptHandler={() => this.setState({view: 'input'})}
+		decryptHandler={() => this.setState({view: 'input'})}
+		    />;
+		break;
+	    case 'input':
+		view = <InputView beginHandler={() => this.setState({view: 'story'})}/>;
+		break;
+	    case 'story':
+		view = <StoryView finishHandler={() => this.setState({view: 'title'})}/>;
+		break;
+	}
+	return (
+	    <div className='view'>
+	    {view}
+	    </div>
+	);
+    }
+}
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-	<CipherStory/>
+	<ViewController/>
       </header>
     </div>
   );
--- src/StoryView.js
+++ src/StoryView.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import TitleView from './TitleScene';
-import InputView from './InputScene';
+import TitleView from './TitleView';
+import InputView from './InputView';
 
 
 class StoryView extends React.Component {
     constructor(props) {
 	super(props);
-	this.state = {
-	    scene: "title",
-	}
     }
 
     render() {
-	let scene;
-	switch(this.state.scene) {
-	    case 'title':
-		scene = <TitleView encryptHandler={() => this.setState({scene: 'input'})}/>;
-		break;
-	    case 'input':
-		scene = <InputView beginHandler={() => this.setState({scene: 'firstop'})}/>;
-		break;
-	    case 'firstop':
-		break;
-	}
 	return (
 	    <div className='story'>
 	    <div className='scenectl'> {'\u2190'} </div>
-	    {scene}
+	    <div className='scene'>wew</div>
 	    <div className='scenectl'> {'\u2192'} </div>
 	    </div>
 	);
