ccee66b1152fc4aec2bdef771e293f786c8a8d51
Zach Lefin <zachlefin@gmail.com>
2020-08-03 18:14:47 -0400

    init PlayStepScene

---
+++ src/PlayStepScene.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PerformStoryScene extends React.Component {
+    render() {
+	return (
+	    <div>
+	    </div>
+	);
+    }
+}
+
+export default PerformStoryScene;
