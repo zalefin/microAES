989599e455ec712bc12cd2e91b5c88fb1559809b
Zach Lefin <zachlefin@gmail.com>
2020-08-03 22:30:45 -0400

    init story

---
+++ src/story.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export class Story extends React.Component {
+
+}
+
+
+export class EncryptStory extends Story {
+
+}
+
+
+export class DecryptStory extends Story {
+
+}
+
