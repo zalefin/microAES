30e5d0983bb13df7b85135afffe2fc97941c4caa
Zach Lefin <zachlefin@gmail.com>
2020-08-03 22:31:11 -0400

    implement StoryView structure

--- src/StoryView.js
+++ src/StoryView.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import TitleView from './TitleView';
-import InputView from './InputView';
+
+
+class DummyNumScene extends React.Component {
+    render() {
+	return (
+	    <div className='scene'>
+	    <div className='cell'>
+	    {this.props.n}
+	    </div>
+	    </div>
+	);
+    }
+}
 
 
 class StoryView extends React.Component {
     constructor(props) {
 	super(props);
+	this.state = {
+	    si: 0,
+	    story: this.props.story,
+	}
     }
 
+    setScene(i) {
+	this.setState({si: i});
+    }
+
+    nextScene() {
+	let i = this.state.si;
+	if(i == 0) return;
+	this.setScene(i);
+    }
+
+    prevScene() {
+	let i = this.state.si;
+	if(i >= this.state.scenes.length) return;
+	this.setScene(i);
+    }
+
+    renderScene() {
+	let i = this.state.si;
+	return this.state.scenes[i]();
+    }
+
+    handleLeft() { this.prevScene(); }
+    
+    handleRight() { this.nextScene(); }
+
     render() {
+	const leftctl = this.state.si != 0 ? 
+	    <div className='scenectl' onClick={this.handleLeft}> {'\u2190'} </div>
+	    :
+	    <div className='scenectl'></div>
+	const rightctl = this.state.si >= this.state.story.states.length ? 
+	    <div className='scenectl' onClick={this.handleRight}> {'\u2192'} </div>
+	    :
+	    <div className='scenectl'></div>
 	return (
 	    <div className='story'>
-	    <div className='scenectl'> {'\u2190'} </div>
-	    <div className='scene'>wew</div>
-	    <div className='scenectl'> {'\u2192'} </div>
+	    {leftctl}
+	    {this.renderScene()}
+	    {rightctl}
 	    </div>
 	);
     }
