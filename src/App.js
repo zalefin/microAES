72a3be7f60924e9331b4b0e4c7ee506e3b191261
Zach Lefin <zachlefin@gmail.com>
2020-08-12 17:42:57 -0400

    replace w spaces

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import TitleView from './TitleView';
 import InputView from './InputView';
 import StoryView from './StoryView';
+import {FibStory, EncryptStory, DecryptStory} from './story';
+import {str2bin} from './microaes';
 
 class ViewController extends React.Component {
     constructor(props) {
 	super(props);
-	this.state = {view: 'title'};
+	this.state = {
+	    view: 'title',
+	    data: null,
+	    key: null,
+	};
     }
 
     render() {
@@ -15,15 +21,23 @@ class ViewController extends React.Component {
 	switch(this.state.view) {
 	    case 'title':
 		view = <TitleView
-		encryptHandler={() => this.setState({view: 'input'})}
-		decryptHandler={() => this.setState({view: 'input'})}
+		encryptHandler={() => this.setState({view: 'input', encrypt: true})}
+		decryptHandler={() => this.setState({view: 'input', encrypt: false})}
 		    />;
 		break;
 	    case 'input':
-		view = <InputView beginHandler={() => this.setState({view: 'story'})}/>;
+		view = <InputView onFormSubmit={(data, key) => {
+		    this.setState({view: 'story', data: data, key: key})
+		}}/>;
 		break;
 	    case 'story':
-		view = <StoryView finishHandler={() => this.setState({view: 'title'})}/>;
+		console.log(this.state.data, this.state.key)
+		const data = str2bin(this.state.data);
+		const key = str2bin(this.state.key);
+		view = <StoryView 
+		    t_story={this.state.encrypt ? FibStory : DecryptStory} 
+		    i_max={10} 
+		    state_init={data}/>;
 		break;
 	}
 	return (
